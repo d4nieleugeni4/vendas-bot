cd482d4e2033d4421fa260eaae5005fc921e50c0
Daniel Eugenia <165719411+d4nieleugeni4@users.noreply.github.com>
2025-06-22 19:08:41 -0300

    Create bot.config.js

---
+++ config/bot.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  dono: "5524981321901",           // Número do dono (com DDI e DDD)
+  bot: "5511963546094",            // Número do bot (pode ser o mesmo se for pessoal)
+  prefixo: ".",                    // Prefixo dos comandos
+  versao: "1.0.0",                 // Versão do bot
+};
